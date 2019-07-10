3640c0a3da862fa8ca73894e03e043892e271c1e
azilles <zilles@iap.fr>
2019-07-10 12:24:20 +0200

    add new structure

---
+++ lib/python/radio_simus/modules.py
@@ -0,0 +1,139 @@
+import os
+import sys
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+#zen is in GRAND convention in degrees
+#injh = injection height above sea level in m
+#Re = Earth radius in m
+
+#a = np.sqrt((Re + injh)**2. - (Re+GdAlt)**2*np.sin(np.pi-np.deg2rad(zen))**2) -
+#(Re+GdAlt)*np.cos(np.pi-np.deg2rad(zen))
+#zen_inj = np.pi-np.arccos((a**2+(Re+injh)**2-Re**2)/(2*a*(Re+injh)))
+#Xmax_primary = modules._getXmax('proton', 10**float(eny)/1e18, np.deg2rad(zen)) #
+
+
+#approximation based on values from plots for gamma (=e) and protons (=pi) # g/cm2
+#Xmax_height, Xmax_distance = modules._dist_decay_Xmax(zen_inj, injh, Xmax_primary) #
+#d_prime: distance from decay point to Xmax
+
+
+
+# Function to compute impedance from GRAND load
+def compute_ZL(freq, DISPLAY = False, R = 300, C = 6.5e-12, L = 1e-6): # SI UNits: Ohms, Farrads, Henry
+  w = 2*np.pi*freq # Pulsation
+  w2 = w*w
+  # ZC = 1./(i*C*w)
+  # ZL = i*L*w
+  # ZR = R
+  # Analytic formulas for R//C//L
+  deno = (L*L*w2+R*R*(1-L*C*w2)*(1-L*C*w2))
+  RL = R*L*L*w2/deno  # Computed formula
+  XL = R*R*L*w*(1-L*C*w2)/deno  # Computed formula
+  if DISPLAY:
+    plt.figure(1)
+    plt.plot(freq/1e6,RL,label="R$_L$")
+    plt.plot(freq/1e6,XL,label="X$_L$")
+    plt.legend(loc="best")
+    plt.xlabel("Frequency (MHz)")
+    plt.ylabel("Load impedance ($\Omega$)")
+    plt.xlim([min(freq/1e6), max(freq/1e6)])
+    plt.show()
+
+  return RL, XL
+
+def TopoToAntenna(u,alpha,beta): #from coordinates in the topography frame to coordinates in the antenna
+    alpha=alpha*np.pi/180 #around y
+    beta=beta*np.pi/180 #around z
+    cb = np.cos(beta)
+    sb = np.sin(beta)
+    ca = np.cos(alpha)
+    sa = np.sin(alpha)
+    roty = np.array([[ca,0,sa],[0,1,0],[-sa,0,ca]])
+    roty = np.linalg.inv(roty)  # Since we rotate referential, inverse transformation should be applied
+    rotz = np.array([[cb,-sb,0],[sb,cb,0],[0,0,1]])
+    rotz = np.linalg.inv(rotz) # Since we rotate referential, inverse transformation should be applied
+    rotyz=roty.dot(rotz)  # beta and then alpha rotation. This induces a EW component for x arm
+
+    # Now rotate along zp so that we are back with x along NS
+    xarm = [1,0,0]  #Xarm
+    xarmp = rotyz.dot(xarm)  # Rotate Xarm along slope
+    # Compute antrot, angle of NS direction in antenna ref = angle to turn Xarm back to North
+    antrot = math.atan2(xarmp[1],xarmp[0])*180/np.pi
+    cz = np.cos(antrot*np.pi/180)
+    sz = np.sin(antrot*np.pi/180)
+    rotzant = np.array([[cz,-sz,0],[sz,cz,0],[0,0,1]])
+    rotzant = np.linalg.inv(rotzant)
+    rottot = rotzant.dot(rotyz)
+
+    [xp,yp,zp] = rottot.dot(u)
+    return np.array([xp,yp,zp])
+
+
+def _getXmax(primarytype, energy, zen2):
+    # type of primary (electron or pion, energy in EeV, zenith (GRAND) in rad
+    # factor approximated from https://pos.sissa.it/301/1100/pdf
+    
+# current version for tau decays    
+    if primarytype=='electron': # aprroximated by gamma shower
+        a=82.5 # g/cm2
+        c=342.5 #g/cm2
+    if primarytype=='pion':  # pion, kaon .... aprroximated by proton
+        a=62.5 # g/cm2
+        c=357.5 #g/cm2
+        
+# fix for CR (zenith computed @ shower core position
+    if primarytype=='proton' or  primarytype=='iron' or primarytype=='Iron':
+        Re= 6370949 # m, Earth radius
+        injh=100000 # m for CR
+        GdAlt=1500 # actual height of our array aboe sealevel
+        # correction for zenith computed a the point of impact to zenith computed @ injection
+        ab = np.sqrt((Re + injh)**2. - (Re+GdAlt)**2*np.sin(np.pi-zen2)**2) -(Re+GdAlt)*np.cos(np.pi-zen2) 
+        zen2 = np.pi-np.arccos((ab**2+(Re+injh)**2-Re**2)/(2*ab*(Re+injh)))
+        
+        if primarytype=='proton': # pion, kaon .... approximated by proton
+            a=62.5 # g/cm2
+            c=357.5 #g/cm2    
+        if primarytype=='iron' or primarytype=='Iron': # aprroximated by proton
+            a=60 # g/cm2 # just approximated 
+            c=177.5 #g/cm2
+    
+    Xmax= a*np.log10(energy*10**6.)+c # E/EeV* 10**6. to be in TeV
+    #print "Zenith angle (Zhaires convention) @ injection:",zen2*180./np.pi
+    #print "Xmax ", Xmax
+
+    return Xmax#/abs(np.cos(np.pi-zen2)) # TODO: how to correct for slanted shower
+
+def _dist_decay_Xmax(zen2, injh2, Xmax_primary): #zen2: zenith of target shower
+    #% Using isothermal Model
+    rho_0 = 1.225*0.001#; % kg/m3 to 0.001g/cm3: 1g/cm3=1000kg/m3, since X given in g/cm2
+    M = 0.028966#;  %kg/mol - 1000g/mol
+    g = 9.81#; %ms-2
+    T = 288.#; % K
+    R = 8.32#; J/K/mol , J=kg m2/s2
+
+    hD=injh2
+    step=10 #m
+    if hD>10000:
+        step=100 #m
+    Xmax_primary= Xmax_primary#* 10. # g/cm2 to kg/m2: 1g/cm2 = 10kg/m2
+    gamma=np.pi-zen2 # counterpart of where it goes to
+    Re= 6370949 # m, Earth radius
+    X=0.
+    i=0.
+    h=hD
+    ai=0
+    while X< Xmax_primary:
+        i=i+1
+        ai=i*step #100. #m
+        hi= -Re+np.sqrt(Re**2. + ai**2. + hD**2. + 2.*Re*hD - 2*ai*np.cos(gamma) *(Re+hD))## cos(gamma)= + to - at 90dg
+        deltah= abs(h-hi) #(h_i-1 - hi)= delta h
+        h=hi # new height
+        X=X+ rho_0*np.exp(-g*M*hi/(R*T)) * step*100. #(deltah*100) *abs(1./np.cos(np.pi-zen2)) # Xmax in g/cm2, slanted = Xmax, vertical/ cos(theta); density in g/cm3, h: m->100cm, np.pi-zen2 since it is defined as where the showers comes from, abs(cosine) so correct for minus values
+    
+    
+    #print "decay to Xmax: ", ai, " Xmaxheight ", h
+    
+    return h, ai # Xmax_height in m, Xmax_distance in m
---
+++ lib/python/radio_simus/signal_treatment.py
@@ -0,0 +1,273 @@
+###
+### NOTE: AZ corrected read-in of traces, before sometimes trace.T needed to feed-in
+###       added a new didgitization function (other one didnt work for some reason
+###
+
+import numpy as np
+from scipy.signal import butter, lfilter, resample
+from scipy.fftpack import rfft, irfft, rfftfreq
+
+
+__all__ = ["add_noise", "digitization", "filter", "run"]
+
+##########################################################################
+##########################################################################
+
+
+def add_noise(vrms, voltages):
+    """Add normal random noise on voltages
+    inputs : (voltage noise rms, voltages)
+    outputs : noisy voltages (time in ns)
+    """
+    noisy_voltages = np.copy(voltages)
+    noisy_voltages[:,1:] = voltages[:,1:] + \
+        np.random.normal(0, vrms, size=np.shape(voltages[:,1:]))
+    return noisy_voltages
+
+def digitization(voltages, tsampling):
+    """Digitize the voltages at an specific sampling
+    inputs : (voltages, sampling rate)
+    outputs : digitized voltages
+    """
+    dt = round(np.mean(np.diff(voltages.T[0]))) #voltages.T[0, 1] - voltages.T[0, 0]
+    num = len(voltages.T[0, :]) * int(tsampling / dt)
+    print(tsampling, dt)
+    if tsampling % dt != 0:
+        raise ValueError("Sampling time not multiple of simulation time")
+    t_sampled = resample(voltages.T[0, :], num)
+    Vx_sampled = resample(voltages.T[1, :], num)
+    Vy_sampled = resample(voltages.T[2, :], num)
+    Vz_sampled = resample(voltages.T[3, :], num)
+    return np.array([t_sampled, Vx_sampled, Vy_sampled, Vz_sampled])
+
+
+#def Digitization_2(v,TSAMPLING):
+    #"""Digitize the voltages at an specific sampling -- v2
+    #inputs : (voltages, sampling rate)
+    #outputs : digitized voltages (time in ns)
+    #"""
+    #tstep = 1/round(1/(v[0,1] - v[0,0])) # tweak the sh**
+    #ratio=int(round(TSAMPLING/tstep))
+    #SAMPLESIZE = int(len(v[0])/ratio)
+    #vx=np.zeros(SAMPLESIZE)
+    #vy=np.zeros(SAMPLESIZE)
+    #vz=np.zeros(SAMPLESIZE)
+    #tf=np.zeros(SAMPLESIZE)  
+    #ind=np.arange(0,SAMPLESIZE)*ratio
+
+    #if len(ind)>SAMPLESIZE:
+        #ind=ind[0:TSAMPLING]
+    #vx[0:len(ind)]=v[1,ind]
+    #vy[0:len(ind)]=v[2,ind]
+    #vz[0:len(ind)]=v[3,ind]
+    #tf[0:len(ind)]=v[0,ind]
+    #for k in range(len(ind),SAMPLESIZE):
+        #tf[k]=tf[k-1]+TSAMPLING
+    #return np.array([tf, vx, vy, vz])
+
+
+# Filter the voltages at a bandwidth
+################################################################
+def _butter_bandpass_filter(data, lowcut, highcut, fs):
+    """subfunction of filt
+    """
+    b, a = butter(5, [lowcut / (0.5 * fs), highcut / (0.5 * fs)],
+                  btype='band')  # (order, [low, high], btype)
+    return lfilter(b, a, data)
+
+
+def filters(voltages, FREQMIN=50.e6, FREQMAX=200.e6):
+  """ Filter signal v(t) in given bandwidth 
+  Parameters
+  ----------
+   : voltages
+      The array of time (s) + voltage (muV) vectors to be filtered
+   : FREQMIN 
+      The minimal frequency of the bandpass filter (Hz)
+   : FREQMAX: 
+      The maximal frequency of the bandpass filter (Hz)
+      
+  Returns
+  -------
+    numpy array
+        time in ns, Voltages (x,y,z)
+  Raises
+  ------
+  Notes
+  -----
+  At present Butterworth filter only is implemented
+  Examples
+  --------
+  ```
+  >>> from signal_treatment import _butter_bandpass_filter
+  ```
+  """
+  
+  print("ATTENTION: output traces inversed now")
+  t = voltages.T[0]
+  # check whether time in s or ns and correct for it
+  if t[1]-t[0] > 0.1:
+      t*=1e-9 # ns to s
+  v = np.array(voltages.T[1:, :])  # Raw signal
+
+  #fs = 1 / np.mean(np.diff(t))  # Compute frequency step
+  fs = round(1 / np.mean(np.diff(t)))  # Compute frequency step
+  print("Trace sampling frequency: ",fs/1e6,"MHz")
+  nCh = np.shape(v.T)[1]
+  vout = np.zeros(shape=(len(t), nCh))
+  res = t
+  for i in range(nCh):
+        vi = v[i,:]
+        #vout[:, i] = _butter_bandpass_filter(vi, FREQMIN, FREQMAX, fs)
+        res = np.append(res,_butter_bandpass_filter(vi, FREQMIN, FREQMAX, fs))
+  
+  res = np.reshape(res,(nCh+1,len(t)))  # Put it back inright format
+  res[0]*=1e9 # s to ns
+  return res.T
+
+#===========================================================================================================
+
+##########################################################################
+#### RUN THE FULL CHAIN
+##########################################################################
+### HARDCODED - TODO to be substituted
+tsampling = 2 # ns
+Vrms = 28 #muV before filtering - NOTE: should be substituted by function returning the value
+
+#===========================================================================================================
+def run(efield, zenith_sim, azimuth_sim, alpha_sim=0., beta_sim=0., DISPLAY=1):
+        ''' 
+        Do the full chain once:
+        1. READ IN THE SIMULATED ELECTRIC FIELD TRACE (at higher level) at hand over as parameter
+        2. APPLY ANTENNA RESPONSE
+        3. ADD STATIONARY NOISE (GALACTIC AND GROUND), VRMS(50-200MHz)= 15muV
+        4. FILTER THE TRACE TO THE 50-200MHz WINDOW
+        5. DIGITIZATION -- 2ns 
+        
+        TODO: make it modular so that people can pick the steps they need
+    
+        
+        Arguments:
+        ----------
+        efield: np array
+            electric field trace
+        zenith_sim: float
+            zenith of shower in deg (GRAND)
+        azimuth_sim: float
+            azimuth of shower in deg (GRAND)
+        alpha, beta: float
+            antenna angles, optional, in deg
+        DISPLAY: 0,1
+            Plotting option off/on
+        
+        Returns:
+        ---------
+        trace:
+            voltage trace, numpy array: time in ns, voltages (x,y,z)
+        
+        '''
+        print("TSampling in ns: ", tsampling, " , Vrms in muV: ", Vrms )
+
+        
+        ### 2. APPLY ANTENNA RESPONSE
+        from computevoltage import get_voltage, compute_antennaresponse
+        trace = compute_antennaresponse(efield, zenith_sim, azimuth_sim, alpha=alpha_sim, beta=beta_sim )
+        
+        #### 2b. deconvolve antenna response - still ongoing work
+        #from invert_computevoltage import compute_electicfield
+        #electric = compute_electicfield(trace, zenith_sim, azimuth_sim, alpha=alpha_sim, beta=beta_sim )
+        
+        if DISPLAY==1:
+            	    
+            plt.figure(1,  facecolor='w', edgecolor='k')
+            plt.subplot(311)
+            plt.plot(efield[0],efield[2], label="Ey = EW")
+            plt.plot(efield[0],efield[1], label="Ex = NS")
+            plt.plot(efield[0],efield[3], label="Ez = UP")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Electric field (muV/m)')
+            plt.legend(loc='best')
+            plt.subplot(312)
+            plt.plot(trace[0]*1e9,trace[2], label="EW")
+            plt.plot(trace[0]*1e9,trace[1], label="NS")
+            plt.plot(trace[0]*1e9,trace[3], label="Vertical")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Voltage (muV)')
+            plt.legend(loc='best')
+            #plt.subplot(313)
+            #plt.plot(electric[0],electric[2], label="Ey = EW")
+            #plt.plot(electric[0],electric[1], label="Ex = NS")
+            #plt.plot(electric[0],electric[3], label="Ez = UP")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Electric field (muV/m)')
+            plt.legend(loc='best')
+            
+            plt.show()
+            #plt.savefig('voltage.png', bbox_inches='tight')
+            
+
+        
+        ### 3. ADD STATIONARY NOISE (GALACTIC AND GROUND), VRMS(50-200MHz)= 15muV
+        #Vrms = 28 #muV before filtering - NOTE: should be substituted by function returning the value
+        trace = add_noise(Vrms, trace) # remove tranposed in signal_treatment
+        
+        if DISPLAY==1:
+            	    
+            plt.figure(2,  facecolor='w', edgecolor='k')
+            plt.plot(trace[0]*1e9,trace[1], label="EW")
+            plt.plot(trace[0]*1e9,trace[2], label="NS")
+            plt.plot(trace[0]*1e9,trace[3], label="Vertical")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Voltage + Noise (muV)')
+            plt.legend(loc='best')
+            
+            plt.show()
+            
+            
+        ### 4. FILTER THE TRACE TO THE 50-200MHz WINDOW
+        trace = filters(trace, FREQMIN=50.e6, FREQMAX=200.e6)
+    
+        
+        if DISPLAY==1:
+            	    
+            plt.figure(3,  facecolor='w', edgecolor='k')
+            plt.subplot(211) # time domain
+            plt.plot(trace[0]*1e9,trace[1], label="EW")
+            plt.plot(trace[0]*1e9,trace[2], label="NS")
+            plt.plot(trace[0]*1e9,trace[3], label="Vertical")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Voltage + Noise (muV) - filtered')
+            plt.legend(loc='best')
+            
+            plt.subplot(212) # frequency domain            
+            freqs  = np.fft.rfftfreq(trace.shape[1],trace[0,1]-trace[0,0])
+            plt.plot(freqs,np.abs(np.fft.rfft(trace[1])), label="EW")
+            plt.plot(freqs,np.abs(np.fft.rfft(trace[2])), label="NS")
+            plt.plot(freqs,np.abs(np.fft.rfft(trace[3])), label="Vertical")
+            plt.xlabel('frequency [Hz]')
+            plt.ylabel('Amplitude')
+            plt.legend(loc='best')
+
+            plt.show()
+        
+        ### 5. DIGITIZATION -- 2ns 
+        trace =digitization(trace,tsampling)
+        
+        if DISPLAY==1:
+            	    
+            plt.figure(4,  facecolor='w', edgecolor='k')
+            plt.plot(trace[0]*1e9,trace[1], label="EW")
+            plt.plot(trace[0]*1e9,trace[2], label="NS")
+            plt.plot(trace[0]*1e9,trace[3], label="Vertical")
+            plt.xlabel('Time (nsec)')
+            plt.ylabel('Voltage + Noise (muV) - digitized')
+            plt.legend(loc='best')
+            
+            plt.show()
+        print(trace)
+
+        return trace
+            
+            
+#===========================================================================================================
+#===========================================================================================================
