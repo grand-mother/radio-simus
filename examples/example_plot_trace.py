071556fbd9ede94174aec6a2a8ae8967706f0efa
azilles <zilles@iap.fr>
2019-11-19 15:09:46 +0100

    reads in now one trace from all-antenna hdf5 file

--- examples/example_plot_trace.py
+++ examples/example_plot_trace.py
@@ -35,6 +35,7 @@ import matplotlib.pyplot as plt
 import pylab
 
 from astropy.table import Table
+from astropy import units as u  
 import h5py
 
 from os.path import split, join, realpath
@@ -43,44 +44,52 @@ sys.path.append(join(root_dir, "lib", "python"))
 import radio_simus 
 from radio_simus.utils import _getAngle
 from radio_simus.signal_treatment import p2p, hilbert_env, hilbert_peak
+from radio_simus.io_utils import _load_to_array, _load_eventinfo_fromhdf
 
 
-# path to folder containing the inp-file, trace files and antpos.dat 
-path = sys.argv[1]
+if __name__ == '__main__':
+
+    if ( len(sys.argv)<2 ):
+        print("""
+        Script to read in a single antenna trace
+        -- Plot trace (PLOT)
+        -- Plot Hilbert envelope (HILB)
+        -- calculate Angle (ANGLE) -- to be fixed for CR (modules.py)
+        
+        Usage: python3 example_plot_trace.py <path to event> <keyword:efield/voltages/...> <antID>
+        example: python3 example_plot_trace.py event_000001.hdf5 efield 9
+
+        """)
+        sys.exit(0)
+
+# path to event/hdf5 file
+path_hdf5 = sys.argv[1]
 # antenna ID
 i = sys.argv[3]
 
+keyword=str(sys.argv[2]) # efield / voltages
+
 # load trace
-if sys.argv[2] == 'e': 
-    # readin electric field trace from astropy table (can be in principle directly plotted as done in in_out) 
-    # and convert its to numpy array for later use 
-    txt = Table.read(path+ 'table_'+str(i)+'.hdf5', path="efield") #txt.T[0]:time in ns, txt.T[1]: North-South, txt.T[2]: East-West, txt.T[3]: Up , all electric field in muV/m
-    trace = np.array([txt['Time'], txt['Ex'], txt['Ey'], txt['Ez']]).T
-    unit='muV/m'
-if sys.argv[2] == 'v': # readin voltage trace and convert to numpy array
-    txt = Table.read(path+ 'table_'+str(i)+'.hdf5', path="voltages")#txt.T[0]: time in ns, txt.T[1]: North-South, txt.T[2]: East-West, txt.T[3]: Up , all voltage in muV
-    trace=np.array([txt['Time'], txt['Vx'], txt['Vy'], txt['Vz']]).T
-    unit='muV (' +str(f1)+'-'+str(f2)+'MHz)'
-### Filtered traces not yet explicitely implemented
-#if sys.argv[2] == 'f': # readin filtered electric field trace
-    #txt = np.loadtxt(path+ 'a'+str(i)+'_'+str(f1)+'-'+str(f2)+'MHz.dat') #txt.T[0]: time in ns, txt.T[1]: North-South, txt.T[2]: East-West, txt.T[3]: Up , all electric field in muV/m
-    #unit='muV/m (' +str(f1)+'-'+str(f2)+'MHz)'
+trace, time_unit, unit, ant_position, ant_slopes = _load_to_array(path_hdf5, content=keyword, ant="/" + str(i) +"/" )
+time_unit=str(time_unit)
+unit=str(unit)
+
 
 
 
 
 ########### How to plot a trace 
-PLOT=False
+PLOT=True
 if PLOT:
     #### Plotting section 
     fig=plt.figure(1,figsize=(8, 5), dpi=120, facecolor='w', edgecolor='k')
 
-    plt.title("antenna ID: "+str(i))
-    plt.plot(trace.T[0], trace.T[1], 'b-', label= "Ex=NS, PtP={0:.2f}".format(p2p(trace.T[1]))+unit )
-    plt.plot(trace.T[0], trace.T[2], 'r-', label= "Ey=EW, PtP={0:.2f}".format(p2p(trace.T[2]))+unit )
-    plt.plot(trace.T[0], trace.T[3], 'g-', label= "Ez=Up, PtP={0:.2f}".format(p2p(trace.T[3]))+unit )
+    plt.title(keyword +", antenna ID: "+str(i))
+    plt.plot(trace.T[0], trace.T[1], 'b-', label= "NS, PtP = {0:.2f} ".format(p2p(trace.T[1]))+unit )
+    plt.plot(trace.T[0], trace.T[2], 'r-', label= "EW, PtP = {0:.2f} ".format(p2p(trace.T[2]))+unit )
+    plt.plot(trace.T[0], trace.T[3], 'g-', label= "Up, PtP = {0:.2f} ".format(p2p(trace.T[3]))+unit )
 
-    plt.xlabel(r"time (ns)", fontsize=16)
+    plt.xlabel(r"time ("+time_unit+")", fontsize=16)
     plt.ylabel(r"Amplitude ("+unit+")", fontsize=16)
     plt.legend(loc='best', fancybox=True)
 
@@ -95,10 +104,10 @@ if HILB:
     fig2=plt.figure(2,figsize=(8, 5), dpi=120, facecolor='w', edgecolor='k')
 
     plt.title("Hilbert env -- antenna ID: "+str(i))
-    plt.plot(trace.T[0], hilbert, 'b-', label= "Ey=EW, Max.={0:.2f}".format(max(hilbert))+unit )
-    plt.plot(trace.T[0], trace.T[2], 'r--', label= "Ey=EW, PtP={0:.2f}".format(p2p(trace.T[2]))+unit )
+    plt.plot(trace.T[0], hilbert, 'b-', label= "EW, Max.= {0:.2f} ".format(max(hilbert))+unit )
+    plt.plot(trace.T[0], trace.T[2], 'r--', label= "EW, PtP = {0:.2f} ".format(p2p(trace.T[2]))+unit )
 
-    plt.xlabel(r"time (ns)", fontsize=16)
+    plt.xlabel(r"time ("+time_unit+")", fontsize=16)
     plt.ylabel(r"Amplitude ("+unit+")", fontsize=16)
     plt.legend(loc='best', fancybox=True)
 
@@ -109,14 +118,20 @@ ANGLE=False
 if ANGLE: # Only for neutrino showers
     from radio_simus.modules import _getXmax, _dist_decay_Xmax, _get_XmaxPosition, _get_CRzenith
     
-    Xmaxpos =  _get_XmaxPosition(txt.meta["primary"],txt.meta["energy"],txt.meta["zenith"], txt.meta["azimuth"], txt.meta["injection_height"])
-    #zen_CR = _get_CRzenith(txt.meta["zenith"],txt.meta["injection_height"], 0.)
-    print("Angle in deg: ", _getAngle(refpos=Xmaxpos ,theta=txt.meta["zenith"],azim=txt.meta["azimuth"],ANTENNAS=txt.meta["position"], core=txt.meta["core"]))
+    #load shower information
+    shower, antID, positions, slopes = _load_eventinfo_fromhdf(path_hdf5)
+    
+    # this does not yet work correctly....
+    Xmaxpos =  _get_XmaxPosition(shower["primary"], shower["energy"].value, 
+                                 shower["zenith"].value, shower["azimuth"].value, shower["injection_height"].value)
+    print(shower, Xmaxpos)
+    #zen_CR = _get_CRzenith(shower["zenith"].value,shower["injection_height"].value, 0.)
+    print("Angle in deg: ", _getAngle(refpos=Xmaxpos, theta=shower["zenith"].value, azim=shower["azimuth"].value, 
+                                      ANTENNAS=positions[np.where(antID==str(i))].value, core=shower["core"].value))
     
     ## explicit example
     #Xmaxpos =  _get_XmaxPosition('electron', 0.5*1e18, 87., 0., 2800.)
     #print(_getAngle(refpos=Xmaxpos ,theta=87.,azim=0,ANTENNAS=np.array([50051,0., 3272]), core=[67407.46,0.,6332.68]))
     
 
-########### call SNR module
-    #### coming soon in 
+
