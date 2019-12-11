94d6498d93f0ffe433a3fbb01640c178c1381fe3
azilles <zilles@iap.fr>
2019-12-11 16:31:13 +0100

    bug fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,10 @@ Unit tests for the radio_simus package
 """
 
 from pathlib import Path
+from os.path import split, join, realpath
+import sys
+root_dir = realpath(join(split(__file__)[0], "..")) # = $PROJECT
+sys.path.append(join(root_dir, "lib", "python"))
 from radio_simus import load_config
 
 path = Path(__file__).parent / "../examples/test.config"
